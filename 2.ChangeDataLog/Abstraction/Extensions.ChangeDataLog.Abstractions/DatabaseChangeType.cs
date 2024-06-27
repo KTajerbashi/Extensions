8387c6eeb5d6c39cf03e1699504bde6cca3b9abe
KTajerbsahi <kamrantajerbashi@gmail.com>
2024-06-27 18:24:22 +0330

    Feature : Add Change Data Log Configuration

---
+++ 2.ChangeDataLog/Abstraction/Extensions.ChangeDataLog.Abstractions/DatabaseChangeType.cs
@@ -0,0 +1,10 @@
+﻿namespace Extensions.ChangeDataLog.Abstractions;
+/// <summary>
+/// نوع تغییری که انجام میشود
+/// </summary>
+public enum DatabaseChangeType
+{
+    Insert = 1,
+    Update = 2,
+    Delete = 3
+}
