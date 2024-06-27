8e9fbd65390d4e114baa6e62eea575d59bde9a6f
KTajerbsahi <kamrantajerbashi@gmail.com>
2024-06-27 19:18:28 +0330

    Feature : Add Caching InMemory

---
+++ 3.Caching/Implemetations/Extensions.Caching.InMemory/Extensions/DependencyInjection/InMemoryCachingServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Extensions.Caching.InMemory.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Extensions.Caching.Abstractions;
+using Extensions.Serializers.Abstractions;
+using Extensions.Serializers.NewtonSoft.Services;
+
+namespace Extensions.Caching.InMemory.Extensions.DependencyInjection;
+
+/// <summary>
+/// در ادامه مرحله چهارم
+/// </summary>
+public static class InMemoryCachingServiceCollectionExtensions
+{
+
+    public static IServiceCollection AddKernelInMemoryCaching(this IServiceCollection services)
+        => services
+        .AddMemoryCache()
+        .AddTransient<ICacheAdapter, InMemoryCacheAdapter>()
+        .AddSNewtonSoftSerializer();
+
+    public static IServiceCollection AddSNewtonSoftSerializer(this IServiceCollection services)
+        => services.AddSingleton<IJsonSerializer, NewtonSoftSerializer>();
+}
