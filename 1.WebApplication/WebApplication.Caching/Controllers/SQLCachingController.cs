076a1c0a4e089ad3ba28c39648a9fc015f18feaf
KTajerbsahi <kamrantajerbashi@gmail.com>
2024-06-28 19:21:40 +0330

    Feature : Add Web APi Applications

---
+++ 1.WebApplication/WebApplication.Caching/Controllers/SQLCachingController.cs
@@ -0,0 +1,35 @@
+﻿using Application.Layer.DataAccess.ChangeDataLog;
+using Extensions.Caching.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationAPI.Controllers.Bases;
+
+namespace WebApplicationAPI.Caching.Controllers;
+
+public class SqlCachingController : BaseController
+{
+    private readonly ICacheAdapter _cacheAdapter;
+    public SqlCachingController(DatabaseContext context, ICacheAdapter cacheAdapter) : base(context)
+    {
+        _cacheAdapter = cacheAdapter;
+    }
+
+    [HttpPost]
+    public IActionResult Add(string Key, string Value)
+    {
+        _cacheAdapter.Add(Key, Value, DateTime.Now.AddDays(1), null);
+        return Ok();
+    }
+
+    [HttpGet]
+    public IActionResult Get(string Key)
+    {
+        return Ok(_cacheAdapter.Get<string>(Key));
+    }
+
+    [HttpDelete]
+    public IActionResult Delete(string Key)
+    {
+        _cacheAdapter.RemoveCache(Key);
+        return Ok();
+    }
+}
