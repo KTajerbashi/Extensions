a291ca8ff5df172f4237ae687ae35774a49eef09
KTajerbsahi <kamrantajerbashi@gmail.com>
2024-06-28 18:56:00 +0330

    Sync

---
+++ 8.ObjectMappers/Implemetations/Extensions.ObjectMappers.AutoMapper/Options/AutoMapperOption.cs
@@ -0,0 +1,9 @@
+﻿namespace Extensions.ObjectMappers.AutoMapper.Options;
+
+/// <summary>
+/// در ادامه مرحله چهارم
+/// </summary>
+public class AutoMapperOption
+{
+    public string AssemblyNamesForLoadProfiles { get; set; } = string.Empty;
+}
