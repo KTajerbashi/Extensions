8002d10d5db5491bb28ef572f8673542a552bcfc
KTajerbashi <kamrantajerbashi@gmail.com>
2024-11-21 02:35:38 +0330

    Feature : add Abstraction Project

---
+++ ObjectMappers/Abstraction/Extensions.ObjectMappers.Abstractions/IMapperAdapter.cs
@@ -0,0 +1,8 @@
+﻿namespace Extensions.ObjectMappers.Abstractions;
+
+public interface IMapperAdapter
+{
+    TDestination Map<TSource, TDestination>(TSource source);
+    IEnumerable<TDestination> Map<TSource, TDestination>(IEnumerable<TSource> source);
+    List<TDestination> Map<TSource, TDestination>(List<TSource> source);
+}
