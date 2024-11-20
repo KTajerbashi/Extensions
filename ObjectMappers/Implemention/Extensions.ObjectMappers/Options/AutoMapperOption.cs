a487153cd434ef4e618143d54eb521fdebd7ea48
KTajerbashi <kamrantajerbashi@gmail.com>
2024-11-21 02:36:02 +0330

    Feature : Add Implementation Version 2

---
+++ ObjectMappers/Implemention/Extensions.ObjectMappers/Options/AutoMapperOption.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions.ObjectMappers.Options;
+
+public class AutoMapperOption
+{
+    public string AssmblyNamesForLoadProfiles { get; set; } = string.Empty;
+}
