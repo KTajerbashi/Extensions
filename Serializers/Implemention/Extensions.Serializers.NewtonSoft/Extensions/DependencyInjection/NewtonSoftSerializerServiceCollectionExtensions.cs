dfa009ba15909aae606cc23f016db9b7635a1f99
KTajerbashi <kamrantajerbashi@gmail.com>
2024-11-21 23:35:53 +0330

    Feature : NewtonSoft Json Added

---
+++ Serializers/Implemention/Extensions.Serializers.NewtonSoft/Extensions/DependencyInjection/NewtonSoftSerializerServiceCollectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Extensions.Serializers.Abstractions;
+using Extensions.Serializers.NewtonSoft.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Extensions.Serializers.NewtonSoft.Extensions.DependencyInjection;
+public static class NewtonSoftSerializerServiceCollectionExtensions
+{
+    public static IServiceCollection AddNewtonSoftSerializer(this IServiceCollection services)
+        => services.AddSingleton<IJsonSerializer, NewtonSoftSerializer>();
+}
