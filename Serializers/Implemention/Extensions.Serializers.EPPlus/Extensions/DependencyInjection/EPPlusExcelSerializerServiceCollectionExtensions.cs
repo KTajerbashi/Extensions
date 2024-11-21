21a03ee148ad7362a2fdc0c85aa55be2d9c27fd6
KTajerbashi <kamrantajerbashi@gmail.com>
2024-11-21 23:35:02 +0330

    Feature : EPPlus Added

---
+++ Serializers/Implemention/Extensions.Serializers.EPPlus/Extensions/DependencyInjection/EPPlusExcelSerializerServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Extensions.Serializers.Abstractions;
+using Extensions.Serializers.EPPlus.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Extensions.Serializers.EPPlus.Extensions.DependencyInjection;
+
+public static class EPPlusExcelSerializerServiceCollectionExtensions
+{
+    public static IServiceCollection AddEPPlusExcelSerializer(this IServiceCollection services)
+        => services.AddSingleton<IExcelSerializer, EPPlusExcelSerializer>();
+}
