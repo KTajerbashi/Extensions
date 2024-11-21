adddcab957059b5e6546699425c719da6f0f1e8a
KTajerbashi <kamrantajerbashi@gmail.com>
2024-11-21 23:35:34 +0330

    Feature : Microsoft Serilizer Added

---
+++ Serializers/Implemention/Extensions.Serializers.Microsoft/Extensions/DependencyInjection/MicrosoftSerializerServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Extensions.Serializers.Abstractions;
+using Extensions.Serializers.Microsoft.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Extensions.Serializers.Microsoft.Extensions.DependencyInjection;
+
+public static class MicrosoftSerializerServiceCollectionExtensions
+{
+    public static IServiceCollection AddMicrosoftSerializer(this IServiceCollection services)
+        => services.AddSingleton<IJsonSerializer, MicrosoftSerializer>();
+}
