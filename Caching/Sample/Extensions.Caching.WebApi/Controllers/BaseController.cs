976ab481f34da795c8850af3c8e0cb258b9f7c49
KTajerbashi <kamrantajerbashi@gmail.com>
2025-03-25 15:07:48 +0330

    Feature : Add Sample Project

---
+++ Caching/Sample/Extensions.Caching.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,33 @@
+﻿using Extensions.Caching.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Extensions.Caching.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public abstract class BaseController : Controller
+{
+    private readonly ICacheAdapter _cacheAdapter;
+    protected BaseController(ICacheAdapter cacheAdapter)
+    {
+        _cacheAdapter = cacheAdapter;
+    }
+    [HttpGet("Add")]
+    public IActionResult Add(string key, string value, DateTime? absoluteExpiration, TimeSpan? slidingExpiration)
+    {
+        _cacheAdapter.Add(key, value, absoluteExpiration, slidingExpiration);
+        return Ok("Added");
+    }
+
+    [HttpGet("Get")]
+    public IActionResult Get(string key)
+    {
+        return Ok(_cacheAdapter.Get<string>(key));
+    }
+    [HttpGet("Remove")]
+    public IActionResult Remove(string key)
+    {
+        _cacheAdapter.RemoveCache(key);
+        return Ok("Removed");
+    }
+}
