5f64123f51c402e642b798df69eeb5cb0f51408d
KTajerbashi <kamrantajerbashi@gmail.com>
2025-03-25 15:06:37 +0330

    Feature : Add Cach InMemory

---
+++ Caching/Implemention/InMemory/Extensions.Caching.InMemory/DependencyInjections.cs
@@ -0,0 +1,11 @@
+﻿using Extensions.Caching.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Extensions.Caching.InMemory;
+public static class DependencyInjections
+{
+    public static IServiceCollection AddInMemoryCaching(this IServiceCollection services)
+        => services.AddMemoryCache().AddTransient<ICacheAdapter, InMemoryCacheAdapter>();
+
+}
+
