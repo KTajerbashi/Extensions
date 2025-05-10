ea383826de82325646ebf590c18eb86d832a5bbe
KTajerbashi <kamrantajerbashi@gmail.com>
2025-05-10 19:44:58 +0330

    Feature : Add Message System

---
+++ MessageBroker/Sample/ConsoleSamples/MessageSystem/ConsoleSample.MessageSystem.Receiver/Program.cs
@@ -0,0 +1,8 @@
+﻿using Framework.Basesource.Extensions;
+using Framework.Basesource.RabbitMQServices;
+
+const string RoutingKey = "MessageSystem";
+const string QueueName = "MessageSystem";
+
+MessageSystem service = new(ApplicationType.Receiver,RoutingKey,QueueName);
+await service.RunAsync();
