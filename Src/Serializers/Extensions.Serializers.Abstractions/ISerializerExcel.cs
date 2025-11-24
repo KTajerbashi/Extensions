562405818c1ebdbbb8fff5627b7a387c78181906
KTajerbashi <kamrantajerbashi@gmail.com>
2025-11-24 21:41:52 +0330

    #Feature Add Serializer Abstraction Json and Excel

---
+++ Src/Serializers/Extensions.Serializers.Abstractions/ISerializerExcel.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+
+namespace Extensions.Serializers.Abstractions;
+
+public interface ISerializerExcel
+{
+    byte[] ListToExcelByteArray<T>(List<T> list, string sheetName = "Result");
+    DataTable ExcelToDataTable(byte[] bytes);
+    List<T> ExcelToList<T>(byte[] bytes) where T : new();
+}
---
+++ Src/Serializers/Extensions.Serializers.Abstractions/ISerializerJson.cs
@@ -0,0 +1,8 @@
+﻿namespace Extensions.Serializers.Abstractions;
+
+public interface ISerializerJson
+{
+    string Serialize<TInput>(TInput input);
+    TOutput Deserialize<TOutput>(string input);
+    object Deserialize(string input, Type type);
+}
