515dbc46cb18652c2310468924db2247f44479f0
KTajerbashi <kamrantajerbashi@gmail.com>
2025-11-28 22:39:39 +0330

    #Feature Add Object Mapper and Sample Project with How to USed and Config on our Project

---
+++ Src/ObjectMapper/ObjectMappersApp/Handler/SampleUsage.cs
@@ -0,0 +1,33 @@
+﻿namespace ObjectMappersApp.Handler;
+
+public class SampleUsage
+{
+    private readonly ServiceHandler _handler;
+
+    public SampleUsage(ServiceHandler handler)
+    {
+        _handler = handler;
+    }
+
+    public void Run()
+    {
+        // Example UserEntity
+        var userEntity = new UserEntity();
+
+        // 1) Convert Entity → DTO
+        UserDTO dto = _handler.ConvertToDto(userEntity);
+
+        // 2) Convert DTO → View
+        UserView view = _handler.ConvertToView(dto);
+
+        // Example list of roles
+        var roles = new List<RoleEntity>
+        {
+            new RoleEntity(),
+            new RoleEntity()
+        };
+
+        // 3) Convert list of Entity → list of View
+        List<RoleView> roleViews = _handler.ConvertRoleToViews(roles);
+    }
+}
---
+++ Src/ObjectMapper/ObjectMappersApp/Handler/ServiceHandler.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using Extensions.ObjectMappers.Abstractions;
+
+namespace ObjectMappersApp.Handler;
+
+public class UserEntity
+{
+    public string Name { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
+public class UserDTO
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
+public class UserView
+{
+    public string Name { get; set; }
+    public string DisplayName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Contact { get; set; }
+    public string Email { get; set; }
+}
+
+public record RoleEntity;
+public record RoleDTO;
+public record RoleView;
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<UserEntity, UserDTO>();
+
+        CreateMap<UserDTO, UserView>()
+            .ForMember(dest => dest.DisplayName, opt => opt.MapFrom(src => src.Name))
+            .ForMember(dest => dest.Contact, opt => opt.MapFrom(src => src.Email));
+    }
+}
+public class RoleProfile : Profile
+{
+    public RoleProfile()
+    {
+        CreateMap<RoleEntity, RoleDTO>();
+        CreateMap<RoleDTO, RoleView>();
+    }
+}
+public class ServiceHandler
+{
+    private readonly IMapperAdapter _mapper;
+
+    public ServiceHandler(IMapperAdapter mapperAdapter)
+    {
+        _mapper = mapperAdapter;
+    }
+
+    public UserDTO ConvertToDto(UserEntity entity)
+        => _mapper.Map<UserEntity, UserDTO>(entity);
+
+    public UserView ConvertToView(UserDTO dto)
+        => _mapper.Map<UserDTO, UserView>(dto);
+
+    public List<RoleView> ConvertRoleToViews(List<RoleEntity> roles)
+        => _mapper.Map<RoleEntity, RoleView>(roles);
+}
