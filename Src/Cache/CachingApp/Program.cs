46e307af111f2045fdd8c7b3914c6f348450a825
KTajerbashi <kamrantajerbashi@gmail.com>
2025-12-13 23:59:41 +0330

    #Feature Add Redis Cache to CacheApp

--- Src/Cache/CachingApp/Program.cs
+++ Src/Cache/CachingApp/Program.cs
@@ -1,7 +1,7 @@
 using CachingApp.Components;
 using CachingApp.Repository;
 using CachingApp.Service;
-using Extensions.Caching.Distributed.Sql;
+using Extensions.Caching.Distributed.Redis;
 using Extensions.Serializers.Microsoft;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,8 +16,17 @@ builder.Services.AddRazorComponents()
 #endregion
 
 #region SQL Cache Config With Add appsetting.json => Cache : Section
-builder.Services.AddScoped<ICacheInspector, SqlCacheInspector>();
-builder.Services.AddSqlDistributedCache(configuration, "Cache");
+//builder.Services.AddScoped<ICacheInspector, SqlCacheInspector>();
+//builder.Services.AddSqlDistributedCache(configuration, "Cache");
+//builder.Services.AddMicrosoftSerializers();
+#endregion
+
+#region Redis
+builder.Services.AddRedisDistributedCache(option =>
+{
+    option.Configuration = "localhost:9191,password=123456";
+    option.InstanceName = "CachingApp";
+});
 builder.Services.AddMicrosoftSerializers();
 #endregion
 
--- Src/Cache/CachingApp/Service/SqlCacheInspector.cs
+++ Src/Cache/CachingApp/Service/SqlCacheInspector.cs
@@ -1,52 +1,52 @@
-﻿using CachingApp.Models;
-using CachingApp.Repository;
-using Extensions.Caching.Distributed.Sql;
-using Microsoft.Data.SqlClient;
-using Microsoft.Extensions.Options;
+﻿//using CachingApp.Models;
+//using CachingApp.Repository;
+//using Extensions.Caching.Distributed.Sql;
+//using Microsoft.Data.SqlClient;
+//using Microsoft.Extensions.Options;
 
-namespace CachingApp.Service;
+//namespace CachingApp.Service;
 
-public class SqlCacheInspector : ICacheInspector
-{
-    private readonly DistributedSqlCacheOptions _options;
+//public class SqlCacheInspector : ICacheInspector
+//{
+//    private readonly DistributedSqlCacheOptions _options;
 
-    public SqlCacheInspector(IOptions<DistributedSqlCacheOptions> options)
-    {
-        _options = options.Value;
-    }
+//    public SqlCacheInspector(IOptions<DistributedSqlCacheOptions> options)
+//    {
+//        _options = options.Value;
+//    }
 
-    public List<CacheRecord> GetAll()
-    {
-        var list = new List<CacheRecord>();
+//    public List<CacheRecord> GetAll()
+//    {
+//        var list = new List<CacheRecord>();
 
-        using var con = new SqlConnection(_options.ConnectionString);
-        con.Open();
+//        using var con = new SqlConnection(_options.ConnectionString);
+//        con.Open();
 
-        var sql = $@"
-            SELECT 
-                [Id], 
-                CONVERT(VARCHAR(MAX), [Value]) as Val,
-                [ExpiresAtTime],
-                [SlidingExpirationInSeconds],
-                [AbsoluteExpiration]
-            FROM [{_options.SchemaName}].[{_options.TableName}]
-            ORDER BY ExpiresAtTime DESC";
+//        var sql = $@"
+//            SELECT 
+//                [Id], 
+//                CONVERT(VARCHAR(MAX), [Value]) as Val,
+//                [ExpiresAtTime],
+//                [SlidingExpirationInSeconds],
+//                [AbsoluteExpiration]
+//            FROM [{_options.SchemaName}].[{_options.TableName}]
+//            ORDER BY ExpiresAtTime DESC";
 
-        using var cmd = new SqlCommand(sql, con);
-        using var reader = cmd.ExecuteReader();
+//        using var cmd = new SqlCommand(sql, con);
+//        using var reader = cmd.ExecuteReader();
 
-        while (reader.Read())
-        {
-            list.Add(new CacheRecord
-            {
-                Key = reader.GetString(0),
-                Value = reader.GetString(1),
-                ExpiresAtTime = reader.GetDateTimeOffset(2),
-                SlidingExpirationInSeconds = reader.IsDBNull(3) ? null : reader.GetInt64(3),
-                AbsoluteExpiration = reader.IsDBNull(4) ? null : reader.GetDateTimeOffset(4)
-            });
-        }
+//        while (reader.Read())
+//        {
+//            list.Add(new CacheRecord
+//            {
+//                Key = reader.GetString(0),
+//                Value = reader.GetString(1),
+//                ExpiresAtTime = reader.GetDateTimeOffset(2),
+//                SlidingExpirationInSeconds = reader.IsDBNull(3) ? null : reader.GetInt64(3),
+//                AbsoluteExpiration = reader.IsDBNull(4) ? null : reader.GetDateTimeOffset(4)
+//            });
+//        }
 
-        return list;
-    }
-}
+//        return list;
+//    }
+//}
